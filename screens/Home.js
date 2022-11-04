b9621c85ebfb13008ad9a46da308e1a75d37468b
manishkcse <manish@roringo.com>
2022-11-04 08:08:10 +0530

    add something in home

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,41 @@
-import { View, Text,  SafeAreaView, ScrollView  } from 'react-native'
-import React from 'react'
+import { View,    SafeAreaView, ScrollView  } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import HeaderTabs from '../components/HeaderTabs'
 import SearchBar from '../components/SearchBar'
 import Categories from '../components/Categories'
-import RestaurantItem from '../components/RestaurantItem'
+import RestaurantItem, { localRestaurants } from '../components/RestaurantItem'
+
+ 
+const YELP_API_KEY =
+"bdRJutLhFAQJ36t7b89CWjHFBU4OKzjt9wvZzcY-nkgmvTqlNMjZWV1eG7iBQ9R74SyfxRg9LWnBAkZY06BtAZAe4d2dfX-2vuX8a1l5V7foctHfX9UKEyoM5ts3YXYx";
+
  
 const Home = () => {
+  const  [resturantData, setResturantData] = useState(localRestaurants)
+  // const [city, setCity] = useState("San Francisco");
+  // const [activeTab, setActiveTab] = useState("Delivery");
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl = "https://api.yelp.com/v3/businesses/search?term=restaurants&location=SanDiego";
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+      .then((res) => res.json())
+      .then((json) => console.log( "manish", json.businesses));
+  };
+
+  useEffect(() => {
+   getRestaurantsFromYelp();
+  }, []);
+
+
+  
+
   return (
    <>
    
@@ -16,7 +46,7 @@ const Home = () => {
       </View>
       <ScrollView showsVerticalScrollIndicator={false}> 
       <Categories/>
-      <RestaurantItem/>
+      <RestaurantItem resturantData={resturantData} />
       </ScrollView>
       </SafeAreaView>
    
